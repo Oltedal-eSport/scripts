791c2a5a4cae918993e6c68f34b508e11471d015
John Vik <john@0x7c5.dev>
2024-12-11 19:39:16 +0100

    Added script for creating new local admin

---
+++ newadmin.ps1
@@ -0,0 +1,6 @@
+params($user, $pass)
+Install-Module -Name LocalAccounts -AllowClobber -Force
+# import-module microsoft.powershell.localaccounts -SkipEditionCheck
+$securePass = ConvertTo-SecureString -String $pass -AsPlainText;
+$user = New-LocalUser -Name $name -Password $securePass
+Add-LocalGroupMember -Group "Administratorer" -Member $user
