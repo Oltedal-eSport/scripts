6cee0381e325fa247e7dde5fe0fabf1099bb91a2
John Vik <john@0x7c5.dev>
2024-12-11 19:51:59 +0100

    Fixed admin creation script

--- newadmin.ps1
+++ newadmin.ps1
@@ -1,4 +1,4 @@
-params($user, $pass)
+params($name, $pass)
 Install-Module -Name LocalAccounts -AllowClobber -Force
 # import-module microsoft.powershell.localaccounts -SkipEditionCheck
 $securePass = ConvertTo-SecureString -String $pass -AsPlainText;
