1cfbe1b21df6c6d78c37a51258fc7943417337b3
John Vik <john@0x7c5.dev>
2025-02-08 19:41:05 +0100

    Improved localuser script

--- newuser.ps1
+++ newuser.ps1
@@ -1,6 +1,9 @@
-param($name, $pass)
+#Run on a local session
+
+$pass = "test";
+$name = "Tobias"
 Install-Module -Name LocalAccounts -AllowClobber -Force
 # import-module microsoft.powershell.localaccounts -SkipEditionCheck
 $securePass = ConvertTo-SecureString -String $pass -AsPlainText;
-$user = New-LocalUser -Name $name -Password $securePass -AccountExpires (Get-Date).AddMinutes(3)
+$user = New-LocalUser -Name $name -Password $securePass
 Add-LocalGroupMember -Group "Brukere" -Member $user
